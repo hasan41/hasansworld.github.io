dd61b837ebf659060e37111fa0caab31cb2462bc
hasan41 <hasan.naseer1470@gmail.com>
2022-11-02 22:28:05 -0400

    It has to work

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,5 +4,6 @@ export const headerData = {
     title: "Hello! I am",
     desciption: "Hello World! Hasan, is a Full-stack Software Developer and YouTuber. He is a new grad from Georgia Tech and majored in Computer Science (AI). He is tech enthusiastic and passionate about Machine Learning, Deep Learning, and transforming different neural networks architecture. ",
     image: 'https://i.ibb.co/MPhB2xh/hasan.jpg',
+    //https://i.ibb.co/qnG4L7n/IMG-1097.jpg
     resumePdf: 'https://docs.google.com/document/d/1OmxnrxHbrfQkL7e3BG40HgSmYCWzNSxo9Wa-kOPVPSc/edit?usp=sharing'
 }
